f2ace2758ca44375b19f9431dfb26cf22c68e471
94mark <90877724+94mark@users.noreply.github.com>
2022-01-22 21:34:12 +0900

    Update PlayerMove.cs
    사용자가 바라보는 시선 방향으로 이동 처리

--- VR_TowerDefense/Assets/Scripts/PlayerMove.cs
+++ VR_TowerDefense/Assets/Scripts/PlayerMove.cs
@@ -23,6 +23,8 @@ public class PlayerMove : MonoBehaviour
         float v = ARAVRInput.GetAxis("Vertical");
         //2. 방향을 만든다
         Vector3 dir = new Vector3(h, 0, v);
+        //2.0 사용자가 바라보는 방향으로 입력 값 변화시키기
+        dir = Camera.main.transform.TransformDirection(dir);
         //2-1. 중력을 적용한 수직 방향 추가 v = v0 + at
         yVelocity += gravity * Time.deltaTime;
         //2-2. 바닥에 있을 경우, 수직 항력을 처리하기 위해 속도를 0으로 한다
