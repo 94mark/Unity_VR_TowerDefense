01c10975ffe6751f393f60a3c1f7d4b217e81c54
94mark <90877724+94mark@users.noreply.github.com>
2022-01-22 21:05:55 +0900

    Update PlayerMove.cs
    수직항력 개념 적용
    yVelocity값의 계속된 누적되는 문제 해결

--- VR_TowerDefense/Assets/Scripts/PlayerMove.cs
+++ VR_TowerDefense/Assets/Scripts/PlayerMove.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    //이동 속도
-    public float speed = 5;
-    //CharacaterController 컴포넌트
-    CharacterController cc;
-    
+    public float speed = 5; //이동 속도
+    CharacterController cc; //CharacaterController 컴포넌트
+    public float gravity = -20; //중력 가속도의 크기
+    float yVelocity = 0; //수직 속도
+
     void Start()
     {
         cc = GetComponent<CharacterController>();
@@ -22,6 +22,15 @@ public class PlayerMove : MonoBehaviour
         float v = ARAVRInput.GetAxis("Vertical");
         //2. 방향을 만든다
         Vector3 dir = new Vector3(h, 0, v);
+        //2-1. 중력을 적용한 수직 방향 추가 v = v0 + at
+        yVelocity += gravity * Time.deltaTime;
+        //2-2. 바닥에 있을 경우, 수직 항력을 처리하기 위해 속도를 0으로 한다
+        if(cc.isGrounded)
+        {
+            yVelocity = 0;
+        }
+        
+        dir.y = yVelocity;
         //3. 이동
         cc.Move(dir * speed * Time.deltaTime);
     }
