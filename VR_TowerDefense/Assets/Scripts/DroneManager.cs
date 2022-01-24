fbdfe5b9ec4460df3437797d4472f2e01efbabb9
94mark <90877724+94mark@users.noreply.github.com>
2022-01-24 19:49:57 +0900

    Update DroneManager.cs
    랜덤 위치에 드론 생성 및 위치 지정

--- VR_TowerDefense/Assets/Scripts/DroneManager.cs
+++ VR_TowerDefense/Assets/Scripts/DroneManager.cs
@@ -18,12 +18,29 @@ public class DroneManager : MonoBehaviour
 
     void Start()
     {
-        
+        //생성 시간을 랜덤 범위에서 설정
+        createTime = Random.Range(minTime, maxTime);
     }
 
     
     void Update()
     {
-        
+        //1. 시간이 흘러야 한다
+        currentTime += Time.deltaTime;
+        //2. 만약 경과 시간이 생성 시간을 초과했다면
+        if(currentTime > createTime)
+        {
+            //3. 드론 생성
+            GameObject drone = Instantiate(droneFactory);
+            //4. 드론 위치 설정
+            //랜덤으로 spawnPoints 중 하나를 뽑는다
+            int index = Random.Range(0, spawnPoints.Length);
+            //드론의 위치를 랜덤으로 뽑힌 spawnPoint 의 위치로 할당
+            drone.transform.position = spawnPoints[index].position;
+            //5. 경과 시간 초기화
+            currentTime = 0;
+            //6. 생성 시간 재할당
+            createTime = Random.Range(minTime, maxTime);
+        }
     }
 }
